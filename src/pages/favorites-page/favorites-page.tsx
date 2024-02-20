e55d982b104c3c1979d438685a518862a1603750
ivnikonHA <ivnikon@mail.ru>
2024-02-20 15:43:22 +0300

    Выделяет Footer в отдельный компонент

--- src/pages/favorites-page/favorites-page.tsx
+++ src/pages/favorites-page/favorites-page.tsx
@@ -1,3 +1,4 @@
+import Footer from '../../components/footer/footer';
 import Header from '../../components/header/header';
 
 export default function FavoritesPage() : JSX.Element {
@@ -133,11 +134,7 @@ export default function FavoritesPage() : JSX.Element {
           </section>
         </div>
       </main>
-      <footer className="footer container">
-        <a className="footer__logo-link" href="main.html">
-          <img className="footer__logo" src="img/logo.svg" alt="6 cities logo" width="64" height="33" />
-        </a>
-      </footer>
+      <Footer />
     </div>
   );
 }
