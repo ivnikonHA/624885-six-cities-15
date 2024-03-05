ce0ea1ab638a99a7751b6a2e00320f8745e73c8a
ivnikonHA <ivnikon@mail.ru>
2024-03-05 19:59:30 +0300

    Меняет свойство base в конфигурации vite для опубликования

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/624885-six-cities-15',
   plugins: [react()],
   test: {
     globals: true,
