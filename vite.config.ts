d7a61f45318d3fc06d4efb21299ea220be4a4204
ivnikonHA <ivnikon@mail.ru>
2024-03-09 21:49:02 +0300

    Удаляет параметр 'base' из vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 /// <reference types='vitest' />
 /// <reference types='vite/client' />
 
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/624885-six-cities-15',
   plugins: [react()],
   test: {
     globals: true,
