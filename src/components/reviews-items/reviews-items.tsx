674121c38da4dfb2d9035245735a2453ec12935b
ivnikonHA <ivnikon@mail.ru>
2024-03-12 22:19:08 +0300

    Добавляет компонент списка комментариев

---
+++ src/components/reviews-items/reviews-items.tsx
@@ -0,0 +1,42 @@
+import { CommentType } from '../../types/comments';
+
+type ReviewsItemsProps = {
+  comments: CommentType[];
+}
+
+export default function ReviewsItems({ comments }: ReviewsItemsProps): JSX.Element {
+  return (
+    <>
+      {comments.map((comment) => (
+        <li key={comment.id} className="reviews__item">
+          <div className="reviews__user user">
+            <div className="reviews__avatar-wrapper user__avatar-wrapper">
+              <img
+                className="reviews__avatar user__avatar"
+                src={comment.user.avatarUrl}
+                width="54"
+                height="54"
+                alt="Reviews avatar"
+              />
+            </div>
+            <span className="reviews__user-name">
+              {comment.user.name}
+            </span>
+          </div>
+          <div className="reviews__info">
+            <div className="reviews__rating rating">
+              <div className="reviews__stars rating__stars">
+                <span style={{ width: '80%' }}></span>
+                <span className="visually-hidden">Rating</span>
+              </div>
+            </div>
+            <p className="reviews__text">
+              {comment.comment}
+            </p>
+            <time className="reviews__time" dateTime={comment.date.toString()}>{comment.date.toString()}</time>
+          </div>
+        </li>
+      ))}
+    </>
+  );
+}
