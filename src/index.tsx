ad647f75667149fe285d6eb07242d667c59af89e
ivnikonHA <ivnikon@mail.ru>
2024-03-16 14:56:13 +0300

    Подключит Redux к проекту

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 
 import App from './components/app/app';
 import { getMockOffers } from './mocks/offers-mock';
+import { store } from './store';
 import { getRandomNumber } from './utils';
 
-const placesCount = getRandomNumber(1,999);
+const placesCount = getRandomNumber(1, 999);
 const mockOffers = getMockOffers();
 
 const root = ReactDOM.createRoot(
@@ -14,6 +16,8 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App placesCount={placesCount} offers={mockOffers}></App>
+    <Provider store={store}>
+      <App placesCount={placesCount} offers={mockOffers}></App>
+    </Provider>
   </React.StrictMode>
 );
