3019e9a13bb418cfebb1bee237521cb2e96b5d62
ivnikonHA <ivnikon@mail.ru>
2024-03-20 21:20:26 +0300

    Добавим поддержку токенов

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
 
+import { getToken } from './token';
+
 const BACKEND_URL = 'https://15.design.htmlacademy.pro/six-cities';
 const REQUEST_TIMEOUT = 5000;
 
@@ -8,6 +10,17 @@ export const createAPI = (): AxiosInstance => {
     baseURL: BACKEND_URL,
     timeout: REQUEST_TIMEOUT
   });
+
+  api.interceptors.request.use(
+    (config) => {
+      const token = getToken();
+
+      if (token && config.headers) {
+        config.headers['x-token'] = token;
+      }
+      return config;
+    },
+  );
 
   return api;
 };
