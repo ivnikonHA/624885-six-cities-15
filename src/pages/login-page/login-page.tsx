ba7ffa4ff3073142dd0e92b0ce0e8662f9dd93f9
ivnikonHA <ivnikon@mail.ru>
2024-03-21 20:32:24 +0300

    Добавляет авторизацию пользователя

--- src/pages/login-page/login-page.tsx
+++ src/pages/login-page/login-page.tsx
@@ -1,8 +1,32 @@
+import { FormEvent, ReactEventHandler, useState } from 'react';
 import { Helmet } from 'react-helmet-async';
 
 import Header from '../../components/header/header';
+import { useAppDispatch } from '../../hooks/useAppDispatch';
+import { loginAction } from '../../store/api-actions';
+import { AuthData } from '../../types/auth-data';
+
+type ChangeHandler = ReactEventHandler<HTMLInputElement>;
 
 export default function LoginPage() : JSX.Element {
+  const dispatch = useAppDispatch();
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  });
+  const handleFieldChange: ChangeHandler = (evt) => {
+    const { name, value } = evt.currentTarget;
+    setFormData({...formData, [name]: value});
+  };
+  const handleSubmitForm = (evt: FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+    const authData: AuthData = {
+      login: formData.email,
+      password: formData.password
+    };
+    dispatch(loginAction(authData));
+  };
+
   return (
     <div className="page page--gray page--login">
       <Helmet>
@@ -13,16 +37,37 @@ export default function LoginPage() : JSX.Element {
         <div className="page__login-container container">
           <section className="login">
             <h1 className="login__title">Sign in</h1>
-            <form className="login__form form" action="#" method="post">
+            <form onSubmit={handleSubmitForm} className="login__form form" action="">
               <div className="login__input-wrapper form__input-wrapper">
                 <label className="visually-hidden">E-mail</label>
-                <input className="login__input form__input" type="email" name="email" placeholder="Email" required />
+                <input
+                  onChange={handleFieldChange}
+                  className="login__input form__input"
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  value={formData.email}
+                  required
+                />
               </div>
               <div className="login__input-wrapper form__input-wrapper">
                 <label className="visually-hidden">Password</label>
-                <input className="login__input form__input" type="password" name="password" placeholder="Password" required />
+                <input
+                  onChange={handleFieldChange}
+                  className="login__input form__input"
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  value={formData.password}
+                  required
+                />
               </div>
-              <button className="login__submit form__submit button" type="submit">Sign in</button>
+              <button
+                className="login__submit form__submit button"
+                type="submit"
+              >
+                Sign in
+              </button>
             </form>
           </section>
           <section className="locations locations--login locations--current">
--- src/services/api.ts
+++ src/services/api.ts
@@ -16,7 +16,7 @@ export const createAPI = (): AxiosInstance => {
       const token = getToken();
 
       if (token && config.headers) {
-        config.headers['x-token'] = token;
+        config.headers['X-Token'] = token;
       }
       return config;
     },
