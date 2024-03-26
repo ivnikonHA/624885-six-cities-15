0230fc51dd15d024e9f7831ead47318fa96bf065
ivnikonHA <ivnikon@mail.ru>
2024-03-26 22:05:18 +0300

    Добавляет OffersSlice

--- src/store/api-actions.ts
+++ src/store/api-actions.ts
@@ -7,30 +7,29 @@ import { AuthData } from '../types/auth-data';
 import { OfferType } from '../types/offers';
 import { AppDispatch } from '../types/state';
 import { UserData } from '../types/user-data';
-import { loadOffers, setLoadingOffersStatus } from './action';
 
 type ApiThunkConfigObject = {
   dispatch: AppDispatch;
   extra: AxiosInstance;
-}
+};
 
-const fetchOffersAction = createAsyncThunk<void, undefined, ApiThunkConfigObject>(
-  'fetchOffers',
-  async (_arg, { dispatch, extra: api }) => {
-    dispatch(setLoadingOffersStatus(true));
-    const { data } = await api.get<OfferType[]>(APIRoute.Offers);
-    dispatch(setLoadingOffersStatus(false));
-    dispatch(loadOffers({ offers: data }));
-  }
-);
+const fetchOffersAction = createAsyncThunk<
+  OfferType[],
+  undefined,
+  ApiThunkConfigObject
+>('fetchOffers', async (_arg, { extra: api }) => {
+  const res = await api.get<OfferType[]>(APIRoute.Offers);
+  return res.data;
+});
 
-const checkAuthAction = createAsyncThunk<UserData, undefined, ApiThunkConfigObject>(
-  'checkAuth',
-  async (_arg, { extra: api }) => {
-    const res = await api.get<UserData>(APIRoute.Login);
-    return res.data;
-  }
-);
+const checkAuthAction = createAsyncThunk<
+  UserData,
+  undefined,
+  ApiThunkConfigObject
+>('checkAuth', async (_arg, { extra: api }) => {
+  const res = await api.get<UserData>(APIRoute.Login);
+  return res.data;
+});
 
 const loginAction = createAsyncThunk<UserData, AuthData, ApiThunkConfigObject>(
   'login',
