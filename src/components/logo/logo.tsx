64e498bfe0261e2102caf25fdfb832f6b6ba582f
ivnikonHA <ivnikon@mail.ru>
2024-04-02 21:21:31 +0300

    Мемоизирует логотип

--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -1,10 +1,15 @@
+import { memo } from 'react';
 import { Link } from 'react-router-dom';
 
 import { AppRoute } from '../../const';
 
-export default function Logo() : JSX.Element {
+function Logo() : JSX.Element {
   return (
     <Link to={AppRoute.Root} className="header__logo-link header__logo-link--active">
       <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
     </Link>);
 }
+
+const MemoizedLogo = memo(Logo);
+
+export default MemoizedLogo;
