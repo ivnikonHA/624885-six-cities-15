7456ed4f2cf0532f7443370f02507fe8d50ce5f8
ivnikonHA <ivnikon@mail.ru>
2024-04-03 10:47:57 +0300

    Показывает компонент MainEmpty при отсутствии предложений

---
+++ src/components/main-empty/main-empty.tsx
@@ -0,0 +1,19 @@
+import { useAppSelector } from '../../hooks/useAppSelector';
+import { getCurrentCity } from '../../store/selectors/offers-selectors';
+
+function MainEmpty(): JSX.Element {
+  const currentCity = useAppSelector(getCurrentCity);
+  return (
+    <div className="cities__places-container cities__places-container--empty container">
+      <section className="cities__no-places">
+        <div className="cities__status-wrapper tabs__content">
+          <b className="cities__status">No places to stay available</b>
+          <p className="cities__status-description">We could not find any property available at the moment in {currentCity.name}</p>
+        </div>
+      </section>
+      <div className="cities__right-section"></div>
+    </div>
+  );
+}
+
+export default MainEmpty;
