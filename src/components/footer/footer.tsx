4a29b184ba597525c0ed211e0e547860fbf3c28f
ivnikonHA <ivnikon@mail.ru>
2024-04-08 22:38:13 +0300

    Исправляет ссылку в Footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,11 +1,14 @@
 import { memo } from 'react';
+import { Link } from 'react-router-dom';
+
+import { AppRoute } from '../../const';
 
 function Footer() : JSX.Element {
   return (
     <footer className="footer container">
-      <a className="footer__logo-link" href="main.html">
+      <Link className="footer__logo-link" to={AppRoute.Root}>
         <img className="footer__logo" src="img/logo.svg" alt="6 cities logo" width="64" height="33" />
-      </a>
+      </Link>
     </footer>);
 }
 
