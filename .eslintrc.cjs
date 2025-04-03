faa2eebf91f79e547ed244754a9f199acabcab43
ivnikonHA <ivnikon@mail.ru>
2025-04-03 21:42:18 +0300

    Updates

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+/* eslint-env node */
+
+module.exports = {
+  env: { browser: true, es2022: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    "htmlacademy/react-typescript",
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module', project: 'tsconfig.json' },
+  settings: { react: { version: 'detect' } },
+  plugins: ['react-refresh', 'simple-import-sort'],
+  rules: {
+    'react-refresh/only-export-components': 'warn',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error'
+  },
+  overrides: [
+    {
+      files: [ '*test*' ],
+      rules: { '@typescript-eslint/unbound-method': 'off' }
+    },
+  ],
+}
